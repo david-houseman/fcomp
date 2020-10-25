c0ab51d6b86a9bb4fdc3a59c5b20f13f74e58445
David Houseman <david@shuttle>
2020-10-25 16:17:47 +1100

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+for req in */requirements-dpkg.txt
+do
+    cat req | xargs apt-get -y --no-install-recommends install req
+done
+
+for req in */requirements-py.txt
+do
+    pip3 install -r req
+done
+
