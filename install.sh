ca88b2eff997639dc69cc1f3d705a7c006395eb5
David Houseman <david@shuttle>
2020-10-25 17:49:02 +1100

    Add wheel to requirements.py.

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ do
     cat "$req" | xargs apt-get -y --no-install-recommends install
 done
 
-for req in */requirements-py.txt
+for req in requirements-py.txt */requirements-py.txt
 do
-    pip3 install -r "$req"
+    pip3 install --upgrade -r "$req"
 done
 
 cp /usr/share/zoneinfo/Australia/Sydney /etc/localtime
