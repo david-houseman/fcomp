6069b853d8fee780ff59f7234c610f3894fc2579
David Houseman <david@shuttle>
2020-10-26 12:55:13 +1100

    Display current git commit.

--- dash/app.py
+++ dash/app.py
@@ -22,6 +22,24 @@ app = dash.Dash(
     name=__name__, server=server, external_stylesheets=[dbc.themes.BOOTSTRAP]
 )
 
+def component_git_version():
+
+    git_shorthash = "Unknown"
+    git_time = "00:00"
+    git_author = "Unknown"
+
+    git_output = os.popen(
+        "git show --no-patch --format='%h%n%ai%n%an'"
+    ).read().splitlines()
+
+    if len(git_output) == 3:
+        git_shorthash = git_output[0]
+        git_time = git_output[1]
+        git_author = git_output[2]
+
+    return html.P("{}: {} by {}".format(git_shorthash, git_time, git_author))
+        
+
 def content():
     maxlen = 256
     return [
@@ -62,6 +80,7 @@ def content():
         dbc.Button("Submit", id="submit-button"),
         dbc.Jumbotron([html.Div(id="submit-feedback")]),
         html.Hr(),
+        component_git_version(),
     ]
 
 
